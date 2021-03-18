b1e224678c77620e4f9e8ef99fd115c6223e16ec
ademcatamak <ademcatamak@gmail.com>
2021-03-18 12:11:41 +0300

    Get-Account

--- Src/AccountModule/RIG.AccountModule.Infrastructure/Db/Repositories/AccountRepository.cs
+++ Src/AccountModule/RIG.AccountModule.Infrastructure/Db/Repositories/AccountRepository.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -6,6 +7,7 @@ using RIG.AccountModule.Domain;
 using RIG.AccountModule.Domain.Exceptions;
 using RIG.AccountModule.Domain.Repositories;
 using RIG.AccountModule.Domain.ValueObjects;
+using RIG.Shared.Domain.Pagination;
 using RIG.Shared.Infrastructure.Db;
 
 namespace RIG.AccountModule.Infrastructure.Db.Repositories
@@ -32,6 +34,26 @@ namespace RIG.AccountModule.Infrastructure.Db.Repositories
 
             if (account == null) throw new AccountNotFoundException();
             return account;
+        }
+
+        public async Task<Account> GetByAccountIdAsync(AccountId accountId, CancellationToken cancellationToken)
+        {
+            Account? account = await _appDbContext.Set<Account>()
+                                                  .Where(acc => Equals(acc.Id, accountId))
+                                                  .FirstOrDefaultAsync(cancellationToken);
+
+            if (account == null) throw new AccountNotFoundException();
+            return account;
+        }
+
+        public async Task<PaginatedCollection<Account>> GetAsync(int offset, int limit, CancellationToken cancellationToken)
+        {
+            (int totalItemCount, List<Account> accountList) = await _appDbContext.Set<Account>()
+                                                                                 .PaginatedQueryAsync(offset, limit, cancellationToken);
+
+            if (!accountList.Any()) throw new AccountNotFoundException();
+
+            return new PaginatedCollection<Account>(totalItemCount, accountList);
         }
     }
 }
--- Src/RIG.WebApi/Modules/AccountModule/Controllers/AccountController.cs
+++ Src/RIG.WebApi/Modules/AccountModule/Controllers/AccountController.cs
@@ -1,13 +1,18 @@
 using System;
+using System.Linq;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using RIG.AccountModule.Application.Commands;
 using RIG.AccountModule.Domain;
 using RIG.AccountModule.Domain.ValueObjects;
+using RIG.Shared.Domain.Exceptions;
+using RIG.Shared.Domain.Pagination;
 using RIG.Shared.Infrastructure;
 using RIG.WebApi.Modules.AccountModule.Controllers.Requests;
+using RIG.WebApi.Modules.AccountModule.Controllers.Responses;
 
 namespace RIG.WebApi.Modules.AccountModule.Controllers
 {
@@ -33,6 +38,59 @@ namespace RIG.WebApi.Modules.AccountModule.Controllers
             AccountId accountId = await _executionContext.ExecuteAsync(createAccountCommand, CancellationToken.None);
 
             return StatusCode((int) HttpStatusCode.Created, accountId.Value);
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(PaginatedCollection<AccountHttpResponse>), (int) HttpStatusCode.OK)]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> GetAccounts([FromQuery] GetAccountHttpRequest? getAccountHttpRequest)
+        {
+            var queryAccountCommand = new QueryAccountCommand
+                                      {
+                                          Offset = getAccountHttpRequest?.Offset ?? 0,
+                                          Limit = getAccountHttpRequest?.Limit ?? 1,
+                                      };
+            PaginatedCollection<AccountResponse> paginatedCollection = await _executionContext.ExecuteAsync(queryAccountCommand, CancellationToken.None);
+
+            PaginatedCollection<AccountHttpResponse> result = new PaginatedCollection<AccountHttpResponse>(paginatedCollection.TotalCount,
+                                                                                                           paginatedCollection.Data.Select(a => new AccountHttpResponse
+                                                                                                                                                {
+                                                                                                                                                    AccountId = a.AccountId.Value,
+                                                                                                                                                    Username = a.Username.Value,
+                                                                                                                                                    FirstName = a.Name.FirstName,
+                                                                                                                                                    LastName = a.Name.LastName
+                                                                                                                                                }));
+
+            return StatusCode((int) HttpStatusCode.OK, result);
+        }
+
+        [HttpGet("{accountId}")]
+        [ProducesResponseType(typeof(AccountHttpResponse), (int) HttpStatusCode.OK)]
+        public async Task<IActionResult> GetAccount([FromRoute] Guid accountId)
+        {
+            if (!HttpContext.User.IsInRole("Admin"))
+            {
+                if (!HttpContext.HasAccountId(accountId.ToString()))
+                    throw new ForbiddenException();
+            }
+
+            var queryAccountCommand = new QueryAccountCommand
+                                      {
+                                          Offset = 0,
+                                          Limit = 1,
+                                          AccountId = new AccountId(accountId)
+                                      };
+            PaginatedCollection<AccountResponse> paginatedCollection = await _executionContext.ExecuteAsync(queryAccountCommand, CancellationToken.None);
+            AccountResponse accountResponse = paginatedCollection.Data.First();
+            AccountHttpResponse result = new AccountHttpResponse
+                                         {
+                                             AccountId = accountResponse.AccountId.Value,
+                                             Username = accountResponse.Username.Value,
+                                             FirstName = accountResponse.Name.FirstName,
+                                             LastName = accountResponse.Name.LastName
+                                         };
+
+            return StatusCode((int) HttpStatusCode.OK, result);
         }
     }
 }
